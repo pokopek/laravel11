851e5540496a7801191f6c15cbc1c3a7a82c8005
pokopek <pokopek@gmail.com>
2024-07-14 16:05:33 +0700

    pertama

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+ <!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+<h1>ini contact</h1>
+
+</body>
+</html>
